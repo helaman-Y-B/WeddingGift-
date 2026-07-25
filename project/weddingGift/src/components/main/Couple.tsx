cc5ad54b0335707aeef6d583eb1333e69cdde6a5
Helamã Y. Barbour <90625373+helaman-Y-B@users.noreply.github.com>
2026-07-25 23:11:25 +0000

    Added the images and updated the data json file.
    Still needs to resize the images.

--- project/weddingGift/src/components/main/Couple.tsx
+++ project/weddingGift/src/components/main/Couple.tsx
@@ -2,14 +2,17 @@ import { Link } from "react-router-dom";
 
 export default function Couple() {
 
-  const somethingImage = "/img/something.webp";
+  const somethingImage = "/img/couple-large.jpeg";
 
   return (
     <>
       <div id="coupleInfo">
         <img src={somethingImage} alt="Couples image" />
         <div id="infos">
-          <h2>Couples name</h2>
+          <div id="brinca">
+             <h2>Nicole e João</h2>
+             <p>Nicão</p>
+          </div>
           <p>Venha conhecer a nossa <Link to="/history">História!</Link></p>
         </div>
       </div>
