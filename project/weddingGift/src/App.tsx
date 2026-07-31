cc559ecc8aecc9041612f391d8708ce0f4b43588
Helamã Y. Barbour <90625373+helaman-Y-B@users.noreply.github.com>
2026-07-31 01:51:02 +0000

    Added grid and resized the images of the couple.

--- project/weddingGift/src/App.tsx
+++ project/weddingGift/src/App.tsx
@@ -15,29 +15,31 @@ function App() {
     <>
       <BrowserRouter>
         <Header />
-        <Suspense fallback={<div>Carregando...</div>}>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <Couple />
-                  <div id="cotasInfo">
-                    <h2>As cotas</h2>
-                    <p>
-                      Você gostaria de ajudar este maravilhoso casal com os seus
-                      futuros planos? Abaixo temos as cotas para que você
-                      consiga ajudar e participar da história deles!
-                    </p>
-                  </div>
-                  <Products />
-                </>
-              }
-            />
-            <Route path="/product/:id" element={<ProductDetail />} />
-            <Route path="/history" element={<CouplesHistory />} />
-          </Routes>
-        </Suspense>
+        <main>
+          <Suspense fallback={<div>Carregando...</div>}>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <>
+                    <Couple />
+                    <div id="cotasInfo">
+                      <h2>As cotas</h2>
+                      <p>
+                        Você gostaria de ajudar este maravilhoso casal com os
+                        seus futuros planos? Abaixo temos as cotas para que você
+                        consiga ajudar e participar da história deles!
+                      </p>
+                    </div>
+                    <Products />
+                  </>
+                }
+              />
+              <Route path="/product/:id" element={<ProductDetail />} />
+              <Route path="/history" element={<CouplesHistory />} />
+            </Routes>
+          </Suspense>
+        </main>
         <Footer />
       </BrowserRouter>
     </>
--- project/weddingGift/src/components/main/Couple.tsx
+++ project/weddingGift/src/components/main/Couple.tsx
@@ -1,20 +1,36 @@
 import { Link } from "react-router-dom";
 
 export default function Couple() {
-
-  const somethingImage = "/img/couple-large.jpeg";
-
   return (
     <>
       <div id="coupleInfo">
-        <img src={somethingImage} alt="Couples image" />
+        <picture>
+          <source
+            srcSet="/img/couple-large-l.webp"
+            media="(min-width: 800px)"
+          />
+          <source
+            srcSet="/img/couple-large-m.webp"
+            media="(min-width: 700px)"
+          />
+          <source
+            srcSet="/img/couple-large-s.webp"
+            media="(min-width: 520px)"
+          />
+          <img src="/img/couple-small.webp" alt="Couples image" />
+        </picture>
         <div id="infos">
           <div id="brinca">
-             <h2>Nicole e João</h2>
-             <p>Nicão</p>
+            <h2>Nicole e João</h2>
+            <p>Nicão</p>
           </div>
-          <p>Gostaria de participar deste momento conosco? <a href="http://">Save the Date!</a></p>
-          <p>Venha conhecer a nossa <Link to="/history">História!</Link></p>
+          <p>
+            Gostaria de participar deste momento conosco?{" "}
+            <a href="http://">Save the Date!</a>
+          </p>
+          <p>
+            Venha conhecer a nossa <Link to="/history">História!</Link>
+          </p>
         </div>
       </div>
     </>
